d6f92548ab3c6912538528cf5fd5de0ad62cbef4
root <root@supercode.localdomain>
2022-09-07 10:56:36 +0300

    initing

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,16 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Backpack\CRUD\app\Models\Traits\CrudTrait; // <------------------------------- this one
+use Spatie\Permission\Traits\HasRoles;// <---------------------- and this one
+
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use CrudTrait; // <----- this
+    use HasRoles; // <-
 
     /**
      * The attributes that are mass assignable.
