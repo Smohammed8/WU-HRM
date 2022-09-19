79372a3a222983f0ab16bf5f98378b712982a813
Smohammed8 <seidm9815@gmail.com>
2022-09-19 17:08:17 +0300

    updated validation

--- app/Http/Requests/UnitRequest.php
+++ app/Http/Requests/UnitRequest.php
@@ -25,7 +25,6 @@ class UnitRequest extends FormRequest
     public function rules()
     {
         return [
-<<<<<<< HEAD
             'name'=>'required|regex:/^[a-z A-Z]+$/u|min:5|max:30',
             'acronym' =>'required|regex:/^[a-z A-Z]+$/u|min:5|max:10',
             'email'=>'required|email',
@@ -45,29 +44,6 @@ class UnitRequest extends FormRequest
             'reports_to_id' =>'required|regex:/^[a-z A-Z]+$/u|min:5|max:30',
             'organization_id' =>'required',
             'chair_man_type_id'=>'required',
-=======
-            // 'name' => 'required|min:5|max:255'
-
-            // 'name'=>'required|regex:/^[a-zA-Z]+$/u|min:5|max:30',
-            // 'acronym' =>'required|regex:/^[a-zA-Z]+$/u|min:5|max:10',
-            // 'email'=>'required|email',
-            // 'telephone' =>'required|numeric|digits:10',
-            // 'extension_line' =>'required|regex:/^[a-zA-Z]+$/u|min:5|max:50',
-            // 'location' =>'required|regex:/^[a-zA-Z]+$/u|min:5|max:30',
-            // 'teter' =>'required|image|mimes:png,jpg,jpeg,gif,svg|max:2048|dimensions:min_width=50,min_height=50',
-            // 'seal' =>'required|image|mimes:png,jpg,jpeg,gif,svg|max:2048|dimensions:min_width=50,min_height=50',
-            // 'vision'=>'required|regex:/^[a-zA-Z]+$/u|min:5|max:30',
-            // 'mission'=>'required|regex:/^[a-zA-Z]+$/u|min:5|max:30',
-            // 'objective' =>'required|regex:/^[a-zA-Z]+$/u|min:5|max:30',
-            // 'building_number'=>'required',
-            // 'office_number'=>'required',
-            // 'motto'=>'required|regex:/^[a-zA-Z]+$/u|min:5|max:30',
-            // 'value_list'=>'required|regex:/^[a-zA-Z]+$/u|min:10|max:255',
-            // 'parent_unit_id'=>'required',
-            // 'reports_to_id' =>'required|regex:/^[a-zA-Z]+$/u|min:5|max:30',
-            // 'organization_id' =>'required',
-            // 'chair_man_type_id'=>'required',
->>>>>>> origin/abdi
         ];
     }
 
