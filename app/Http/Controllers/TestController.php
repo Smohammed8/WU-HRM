d4e5e775b7edf49e110f31ef3a9903ccb7f21723
milkyseifu <milkyseifuban@gmail.com>
2022-11-28 04:16:28 -0800

    ms

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,19 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use App\Models\PlacementChoice;
+use App\Models\Position;
 use App\Score\ExperienceScore;
+use App\Score\Score;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
     public function experienceScore()
     {
-        $palcementChoices = PlacementChoice::all();
-        foreach ($palcementChoices as $key => $palcementChoice) {
-            $a = new ExperienceScore();
-            $score = $a->getExperinceScore($palcementChoice);
+        $a = new Score();
+        $psotions = Position::all();
+        foreach ($psotions as $key => $position) {
+            $employee = Employee::all()[1];
+            $eligibility = $a->checkIfExperienceLevel($position, $employee);
+
+            dd($eligibility);
         }
+        // $palcementChoices = PlacementChoice::all();
+        // foreach ($palcementChoices as $key => $palcementChoice) {
+        //     $a = new ExperienceScore();
+        //     $score = $a->getExperinceScore($palcementChoice);
+        // }
         // dd($score);
     }
 }
