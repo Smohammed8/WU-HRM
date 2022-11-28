6113767edc9ca8f7ee966df6d22a4f34a2f4b0bc
milkyseifu <milkyseifuban@gmail.com>
2022-11-28 04:16:10 -0800

    cls

--- app/Score/ExperienceScore.php
+++ app/Score/ExperienceScore.php
@@ -12,43 +12,36 @@ use App\Models\PositionValue;
 use Carbon\Carbon;
 
 class ExperienceScore{
-    public function getExperinceScore()
+    public function getExperinceScore($placementChoice)
     {
-        $employees = Employee::all();
-        $placementChoices = PlacementChoice::all();
-        $valArr = [];
-        foreach ($employees as $key => $employee) {
-            $score = 0;
-            $employeeExperience = Carbon::now()->diff(Carbon::parse($employee->employement_date))->y;
-            $employeeChoice = $employee->choices->first();
-            $employeeeFirstChoice = $employeeChoice->choiceOne;
-            $employeeeSecondChoice = $employeeChoice->choiceTwo;
+        $score = 0;
+        $employeeExperience = Carbon::now()->diff(Carbon::parse($placementChoice->employee->employement_date))->y;
 
-            $positionType = $employeeeFirstChoice->jobTitle->positionType;
-            $requirement = PositionRequirement::where('name', Constants::EXPERIENCE_CRITERIA)->first();
-
-            $positionValue = PositionValue::where('position_type_id', $positionType->id)->where('position_requirement_id', $requirement->id)->first();
+        $employeeeFirstChoice = $placementChoice->choiceOne;
+        $employeeeSecondChoice = $placementChoice->choiceTwo;
+        $positionType = $employeeeFirstChoice->jobTitle->positionType;
+        $requirement = PositionRequirement::where('name', Constants::EXPERIENCE_CRITERIA)->first();
+        
+        $positionValue = PositionValue::where('position_type_id', $positionType->id)->where('position_requirement_id', $requirement->id)->first();
             
-            $experienceCriterias = ExperienceComparisonCriteria::where('position_value_id', $positionValue->id)->get();
+        $experienceCriterias = ExperienceComparisonCriteria::where('position_value_id', $positionValue->id)->get();
 
-            foreach ($experienceCriterias as $key => $experienceCriteria) {
-                if (!$experienceCriteria->max_year) {
-                    $minYear = $experienceCriteria->min_year;
-                    if ($employeeExperience > $minYear) {
-                        $score = $experienceCriteria->value;
-                    }
-                }
-                else{
-                    $minYear = $experienceCriteria->min_year;
-                    $maxYear = $experienceCriteria->max_year;
-
-                    if ($minYear < $employeeExperience && $employeeExperience < $maxYear) {
-                        $score = $experienceCriteria->value;
-                    }
+        foreach ($experienceCriterias as $key => $experienceCriteria) {
+            if (!$experienceCriteria->max_year) {
+                $minYear = $experienceCriteria->min_year;
+                if ($employeeExperience > $minYear) {
+                    $score = $experienceCriteria->value;
                 }
             }
-            $valArr[$employee->id] = $score;
+            else{
+                $minYear = $experienceCriteria->min_year;
+                $maxYear = $experienceCriteria->max_year;
+
+                if ($minYear < $employeeExperience && $employeeExperience < $maxYear) {
+                    $score = $experienceCriteria->value;
+                }
+            }
         }
-        return $valArr;
+        return $score;
     }
 }
